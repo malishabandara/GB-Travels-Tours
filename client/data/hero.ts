beac435d56565cf5d44f86d230056e003adb2479
Malisha Bandara <malishabandara2@gmail.com>
2025-10-14 00:15:28 +0530

    Added images to the hero section

--- client/data/hero.ts
+++ client/data/hero.ts
@@ -1,18 +1,18 @@
 export const heroImages = [
   {
-    src: "https://images.pexels.com/photos/13256066/pexels-photo-13256066.jpeg",
-    alt: "Dubai skyline at night with illuminated highways",
+    src: "./safari/9.jpg",
+    alt: "skyline at night with illuminated highways",
   },
   {
-    src: "https://images.pexels.com/photos/3601425/pexels-photo-3601425.jpeg",
+    src: "./safari/11.jpg",
     alt: "Aerial view of tropical beach and turquoise water",
   },
   {
-    src: "https://images.pexels.com/photos/34239912/pexels-photo-34239912.jpeg",
+    src: "./tour/15.jpg",
     alt: "Golden desert dunes with travelers",
   },
   {
-    src: "https://images.pexels.com/photos/15794381/pexels-photo-15794381.jpeg",
-    alt: "Northern lights above a small church in Iceland",
+    src: "./tour/1.jpg",
+    alt: "Northern lights above a small church in Sri Lanka",
   },
 ];
